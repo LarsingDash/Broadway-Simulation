0887b9487caa1e68c7b524440d54550cbc42c424
LarsingDash <lars.villevoye@gmail.com>
2024-10-03 17:05:29 +0200

    [ADD] FileReaderTemplate Setup

---
+++ Engine/FileReading/FileReaderTemplate.hpp
@@ -0,0 +1,25 @@
+﻿//
+// Created by larsv on 03/10/2024.
+//
+
+#ifndef BROADWAY_SIMULATION_FILEREADERTEMPLATE_HPP
+#define BROADWAY_SIMULATION_FILEREADERTEMPLATE_HPP
+
+#include <iostream>
+
+enum FileType {
+	Map,
+	Artist,
+};
+
+enum SourceType {
+	File,
+	Web,
+};
+
+class FileReaderTemplate {
+	public:
+	static void readFile(const std::string& path, SourceType sourceType, FileType fileType); 
+};
+
+#endif //BROADWAY_SIMULATION_FILEREADERTEMPLATE_HPP

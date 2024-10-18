d251f48dfef2ba521b68de8336e01bdcdbfa59c9
LarsingDash <lars.villevoye@gmail.com>
2024-10-18 14:16:06 +0200

    Keybind structure

--- Engine/Modules/InputModule.cpp
+++ Engine/Modules/InputModule.cpp
@@ -10,28 +10,42 @@
 #include "../CommandHandling/Commands/InfoCommand.hpp"
 
 InputModule::InputModule() {
-	commands[SDL_SCANCODE_SPACE] = std::make_unique<PlayPauseCommand>();
-	commands[SDL_SCANCODE_RETURN] = std::make_unique<TileInteractionCommand>();
-	commands[SDL_SCANCODE_O] = std::make_unique<FileSelectionCommand>();
-	commands[SDL_SCANCODE_A] = std::make_unique<RenderArtistsCommand>();
-	commands[SDL_SCANCODE_LEFT] = std::make_unique<RewindCommand>();
-	commands[SDL_SCANCODE_RIGHT] = std::make_unique<FastForwardCommand>();
-	commands[SDL_SCANCODE_ESCAPE] = std::make_unique<QuitCommand>();
-	commands[SDL_SCANCODE_M] = std::make_unique<InfoCommand>();
+	keys[Commands::PlayPause] = SDL_SCANCODE_SPACE;
+	keys[Commands::TileInteraction] = SDL_SCANCODE_RETURN;
+	keys[Commands::FileSelection] = SDL_SCANCODE_O;
+	keys[Commands::RenderArtists] = SDL_SCANCODE_A;
+	keys[Commands::Rewind] = SDL_SCANCODE_LEFT;
+	keys[Commands::FastForward] = SDL_SCANCODE_RIGHT;
+	keys[Commands::Quit] = SDL_SCANCODE_ESCAPE;
+	keys[Commands::Info] = SDL_SCANCODE_M;
+
+	commandBindings[Commands::PlayPause] = std::make_unique<PlayPauseCommand>();
+	commandBindings[Commands::TileInteraction] = std::make_unique<TileInteractionCommand>();
+	commandBindings[Commands::FileSelection] = std::make_unique<FileSelectionCommand>();
+	commandBindings[Commands::RenderArtists] = std::make_unique<RenderArtistsCommand>();
+	commandBindings[Commands::Rewind] = std::make_unique<RewindCommand>();
+	commandBindings[Commands::FastForward] = std::make_unique<FastForwardCommand>();
+	commandBindings[Commands::Quit] = std::make_unique<QuitCommand>();
+	commandBindings[Commands::Info] = std::make_unique<InfoCommand>();
 }
 
 void InputModule::handleScancode(SDL_Scancode key) {
-	//Ignore everything but the QuitCommand key when FileSelection is opened
-	if (key != SDL_SCANCODE_ESCAPE && SimulationManager::getInstance().guiModule->getFileSelectionFocussed()) return;
+	//Ignore everything but the QuitCommand key when FileSelection is focussed
+	if (key != keys[Commands::Quit] && SimulationManager::getInstance().guiModule->getFileSelectionFocussed()) return;
 
-	auto it = commands.find(key);
-	if (it != commands.end()) {
-		it->second->execute();
+	//Find the command corresponding with the pressed key
+	for (const auto& [command, boundKey]: keys) {
+		if (boundKey == key) {
+			//Call the command
+			commandBindings[command]->execute();
+			break;
+		}
 	}
 }
 
 void InputModule::handleMouseClick() {
+	//Call TileInteraction on mouse click, unless absorbed by a window
 	GUIModule& guiModule = *SimulationManager::getInstance().guiModule;
 	if (!(guiModule.getFileSelectionFocussed() || guiModule.getInfoFocussed()))
-		commands[SDL_SCANCODE_RETURN]->execute();
+		commandBindings[Commands::TileInteraction]->execute();
 }
--- Engine/Modules/InputModule.hpp
+++ Engine/Modules/InputModule.hpp
@@ -2,17 +2,30 @@
 #define INPUT_MODULE_HPP
 
 #include <unordered_map>
+#include <string>
 #include <memory>
 #include <SDL_scancode.h>
 #include "../CommandHandling/Command.hpp"
 
 class InputModule {
-		std::unordered_map<SDL_Scancode, std::unique_ptr<Command>> commands;
-
 	public:
 		InputModule();
         void handleScancode(SDL_Scancode key);
 		void handleMouseClick();
+	private:
+		enum Commands {
+			PlayPause,
+			TileInteraction,
+			FileSelection,
+			RenderArtists,
+			Rewind,
+			FastForward,
+			Quit,
+			Info,
+		};
+		
+		std::unordered_map<Commands, SDL_Scancode> keys;
+		std::unordered_map<Commands, std::unique_ptr<Command>> commandBindings{};
 
 };
 

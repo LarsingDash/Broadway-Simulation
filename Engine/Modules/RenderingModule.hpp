f9ead1b4d44c17c68bb73a4d48eb796a31b9bfbd
Oliver Norton <o.k.norton21@gmail.com>
2024-10-26 18:32:27 +0200

    Rendering module draw method seperation for better readability

--- Engine/Modules/RenderingModule.hpp
+++ Engine/Modules/RenderingModule.hpp
@@ -6,30 +6,36 @@
 #include "PathfindingModule.hpp"
 
 class RenderingModule {
-	public:
-		explicit RenderingModule(SDL_Window* window);
-		~RenderingModule();
-		void init(Museum* mus,ArtistsManager* artM, PathfindingModule* path);
-		void clear();
-		void draw();
-		void drawRectangle(const SDL_Rect& rect, bool isRed);
-		void present();
-		[[nodiscard]] SDL_Renderer* getRenderer() const { return renderer; }
+public:
+    explicit RenderingModule(SDL_Window* window);
+    ~RenderingModule();
+    void init(Museum* mus, ArtistsManager* artM, PathfindingModule* path);
+    void clear();
+    void draw();
+    void drawRectangle(const SDL_Rect& rect, bool isRed);
+    void present();
+    [[nodiscard]] SDL_Renderer* getRenderer() const { return renderer; }
 
-		void toggleRendering();
-		void recalculateTileSize() const;
-		static glm::vec2 tileSize;
-	private:
-		bool isRenderingActive = true;
-		
-		void _red();
-		void _white();
-		void _darkGrey();
-		
-		SDL_Renderer* renderer;
-		Museum* museum{nullptr};
-		ArtistsManager* artistsManager{nullptr};
-		PathfindingModule* pathfindingModule{nullptr};
+    void toggleRendering();
+    void recalculateTileSize() const;
+    static glm::vec2 tileSize;
+private:
+    bool isRenderingActive = true;
+
+    void _red();
+    void _white();
+    void _darkGrey();
+
+    SDL_Renderer* renderer;
+    Museum* museum{nullptr};
+    ArtistsManager* artistsManager{nullptr};
+    PathfindingModule* pathfindingModule{nullptr};
+
+    // New helper methods for drawing components
+    void drawMuseum();
+    void drawArtists();
+    void drawPath();
+    void drawVisitedTiles();
 };
 
 #endif /* RENDERING_MODULE_HPP */
